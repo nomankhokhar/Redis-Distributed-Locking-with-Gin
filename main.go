2ea4ac890a3f1e86253509bca1254906f8be07ed
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-04-25 21:03:31 +0500

    Modify the API's for Status Code

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 )
@@ -24,8 +25,26 @@ func init() {
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	router := gin.Default()
+
+	router.Use(CORSMiddleware())
 
 	router.POST("/api/locktemplate/:id", lockTemplateHandler)
 	router.GET("/api/checklocktemplate/:id", checkLockTemplateHandler)
@@ -36,25 +55,23 @@ func main() {
 	router.Run(":8080")
 }
 
-
-
 func checkLockTemplateHandler(c *gin.Context) {
-    id := c.Param("id")
+	id := c.Param("id")
 
-    // Get expiration time of the locked template
-    expiration, err := rdb.TTL(ctx, id).Result()
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get expiration time"})
-        return
-    }
+	// Get expiration time of the locked template
+	expiration, err := rdb.TTL(ctx, id).Result()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get expiration time"})
+		return
+	}
 
-    if expiration.Seconds() <= 0 {
-        // If expiration time is negative or zero, the template is not locked
-        c.JSON(http.StatusNotFound, gin.H{"error": "template is not locked"})
-        return
-    }
+	if expiration.Seconds() <= 0 {
+		// If expiration time is negative or zero, the template is not locked
+		c.JSON(http.StatusNotFound, gin.H{"error": "template is not locked"})
+		return
+	}
 
-    c.JSON(http.StatusOK, gin.H{"id": id, "time": expiration.Seconds(), "msg": "template locked"})
+	c.JSON(http.StatusOK, gin.H{"id": id, "time": expiration.Seconds(), "msg": "template locked"})
 }
 
 func getAllTemplatesHandler(c *gin.Context) {
@@ -82,25 +99,25 @@ func getAllTemplatesHandler(c *gin.Context) {
 }
 
 func lockTemplateHandler(c *gin.Context) {
-    id := c.Param("id")
+	id := c.Param("id")
 
-    // Check if the key already exists
-    _, err := rdb.Get(ctx, id).Result()
-    if err == nil {
-        c.JSON(http.StatusConflict, gin.H{"error": "template already locked"})
-        return
-    }
+	// Check if the key already exists
+	_, err := rdb.Get(ctx, id).Result()
+	if err == nil {
+		c.JSON(http.StatusOK, gin.H{"id": id, "error": "template already locked"})
+		return
+	}
 
-	expiration := time.Minute
+	expiration := time.Minute * 3
 
-    // Store the ID as both key and value
-    err = rdb.Set(ctx, id, id, expiration).Err() // 0 means no expiration
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to lock template"})
-        return
-    }
+	// Store the ID as both key and value
+	err = rdb.Set(ctx, id, id, expiration).Err() // 0 means no expiration
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to lock template"})
+		return
+	}
 
-    c.JSON(http.StatusOK, gin.H{"id": id, "msg": "template locked successfully"})
+	c.JSON(http.StatusOK, gin.H{"id": id, "msg": "template locked successfully"})
 }
 
 func releaseLockTemplateHandler(c *gin.Context) {
@@ -116,36 +133,36 @@ func releaseLockTemplateHandler(c *gin.Context) {
 }
 
 func increaseLockTemplateHandler(c *gin.Context) {
-    id := c.Param("id")
+	id := c.Param("id")
 
-    // Set expiration time to 1 minute
-    expiration := time.Minute
+	// Set expiration time to 1 minute
+	expiration := time.Minute * 3
 
-    // Check if the template with the provided ID exists
-    exists, err := rdb.Exists(ctx, id).Result()
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to check lock status"})
-        return
-    }
+	// Check if the template with the provided ID exists
+	exists, err := rdb.Exists(ctx, id).Result()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": "failed to check lock status"})
+		return
+	}
 
-    if exists == 0 {
-        c.JSON(http.StatusNotFound, gin.H{"error": "template not found"})
-        return
-    }
+	if exists == 0 {
+		c.JSON(http.StatusOK, gin.H{"error": "template not found"})
+		return
+	}
 
-    // Delete the existing key-value pair
-    err = rdb.Del(ctx, id).Err()
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to delete existing key"})
-        return
-    }
+	// Delete the existing key-value pair
+	err = rdb.Del(ctx, id).Err()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": "failed to delete existing key"})
+		return
+	}
 
-    // Store a new key-value pair with the provided ID and set the expiration time
-    err = rdb.Set(ctx, id, id, expiration).Err()
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to lock template"})
-        return
-    }
+	// Store a new key-value pair with the provided ID and set the expiration time
+	err = rdb.Set(ctx, id, id, expiration).Err()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": "failed to lock template"})
+		return
+	}
 
-    c.JSON(http.StatusOK, gin.H{"id": id, "time": expiration.Seconds(), "msg": "template lock time increased successfully"})
+	c.JSON(http.StatusOK, gin.H{"id": id, "time": expiration.Seconds(), "msg": "template lock time increased successfully"})
 }
