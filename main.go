06a89687c51d16ba81f10d274510ada0de9df6f7
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-04-25 23:27:01 +0500

    feat : Update CORS Error

--- main.go
+++ main.go
@@ -25,18 +25,17 @@ func init() {
 	}
 }
 
-func CORSMiddleware() gin.HandlerFunc {
+func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-ClusterQueueLenght, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
-
 		c.Next()
 	}
 }
@@ -44,7 +43,7 @@ func CORSMiddleware() gin.HandlerFunc {
 func main() {
 	router := gin.Default()
 
-	router.Use(CORSMiddleware())
+	router.Use(CORS())
 
 	router.POST("/api/locktemplate/:id", lockTemplateHandler)
 	router.GET("/api/checklocktemplate/:id", checkLockTemplateHandler)
